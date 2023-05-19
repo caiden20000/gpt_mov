f877e2c54b29a540bed0a456a9684fe0da73e9b2
caiden20000 <caiden2000offline@gmail.com>
2023-05-19 09:27:22 -0400

    Add server.py file

---
+++ server.py
@@ -0,0 +1,12 @@
+
+
+'''
+Plan:
+Test serving HTML, CSS, and JS
+Test serving images
+Test serving audio
+Test serving images, text, and audio at once through JSON?
+Test serving video
+Lay out api endpoints
+
+'''
