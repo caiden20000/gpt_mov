35d3d1a1f65a7bf48f1da447b9ea6a3f0d788df0
caiden20000 <caiden2000offline@gmail.com>
2023-05-25 19:39:42 -0400

    Tweak schema

--- database.py
+++ database.py
@@ -13,12 +13,40 @@ def init_database():
     cur.executescript(script)
     con.commit()
 
+# Straight up delete this function before production
+def clear_database():
+    cur.execute("DROP TABLE users;")
+    cur.execute("DROP TABLE api_keys;")
+    cur.execute("DROP TABLE sequences;")
+    cur.execute("DROP TABLE segments;")
+    cur.execute("DROP TABLE segment_text;")
+    cur.execute("DROP TABLE segment_image;")
+    cur.execute("DROP TABLE segment_audio;")
+
+# TODO: hashing
+def secure_password(username: str , password: str) -> str:
+    return password
+
+# Returns true if username is inserted successfully (ie. username is unique)
+def add_user(username: str, password: str) -> bool:
+    try:
+        cur.execute('''
+                    INSERT INTO users (username, username_case, password)
+                    VALUES (?, ?, ?);
+                    ''', (username.lower(), username, secure_password(username, password)))
+        con.commit()
+    except sqlite3.IntegrityError:
+        return False
+    except Exception as e:
+        print("Error: " + str(e))
+        return False
+    return True
 
 
 def get_username_from_user_id(user_id):
     ret = cur.execute('''
                        SELECT username FROM user
-                       WHERE id = ?
+                       WHERE id = ?;
                        ''', (user_id))
     return ret.fetchall() 
 
@@ -38,5 +66,5 @@ def get_sequence_names_by_user_id(user_id):
 
 
 
-
+init_database()
 
--- schema.sql
+++ schema.sql
@@ -1,57 +1,53 @@
 -- DATABASE SCHEMA for sqlite3
--- ! plural names
+-- Runs on database.py -> init_database()
+-- table names standard plural
 
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS users (
 	id INTEGER PRIMARY KEY,
 	username TEXT UNIQUE COLLATE NOCASE,
-	username_case TEXT,
+	username_case TEXT,				-- Store username with case for display
 	password TEXT 					-- Password will be hashed or something, idk
 );
 
 CREATE TABLE IF NOT EXISTS api_keys (
 	id INTEGER PRIMARY KEY,
-	user_id INTEGER REFERENCES users(id),
+	user_id INTEGER NOT NULL REFERENCES users(id),
 	openai TEXT,					-- Should hash API keys too or..?
 	elevenlabs TEXT
 );
 
 CREATE TABLE IF NOT EXISTS sequences (
 	id INTEGER PRIMARY KEY,
-	user_id INTEGER REFERENCES users(id),
+	user_id INTEGER NOT NULL REFERENCES users(id),
 	name TEXT NOT NULL,
 	script TEXT
 );
 
 CREATE TABLE IF NOT EXISTS segments (
-	id INTEGER NOT NULL,
-	sequence_id INTEGER NOT NULL,
-	index INTEGER NOT NULL,			-- index in the sequence
+	id INTEGER PRIMARY KEY,
+	sequence_id INTEGER NOT NULL REFERENCES sequences(id),
+	sequence_index INTEGER NOT NULL,			-- index in the sequence
 	text_version INTEGER,
 	image_version INTEGER,
-	audio_version INTEGER,
-	PRIMARY KEY (id),
-	FOREIGN KEY (sequence_id) REFERENCES sequences(id)
+	audio_version INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS segment_text (
-	segment_id INTEGER NOT NULL,
+	segment_id INTEGER NOT NULL REFERENCES segments(id),
 	content TEXT NOT NULL,			-- plaintext
-	version INTEGER NOT NULL,
-	FOREIGN KEY (segment_id) REFERENCES segments(id)
+	version INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS segment_image (
-	segment_id INTEGER NOT NULL,
+	segment_id INTEGER NOT NULL REFERENCES segments(id),
 	content TEXT NOT NULL,				-- url
-	version INTEGER NOT NULL,
-	FOREIGN KEY (segment_id) REFERENCES segments(id)
+	version INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS segment_audio (
-	segment_id INTEGER NOT NULL,
+	segment_id INTEGER NOT NULL REFERENCES segments(id),
 	content TEXT NOT NULL,				-- url
-	version INTEGER NOT NULL,
-	FOREIGN KEY (segment_id) REFERENCES segments(id)
+	version INTEGER NOT NULL
 );
