c021b9e9e545c9838e0861b94dd3b6ffd9521956
caiden20000 <caiden2000offline@gmail.com>
2023-05-27 18:44:33 -0400

    Planned interface functions

--- database.py
+++ database.py
@@ -1,4 +1,110 @@
+'''
+Interface:
+
+### Add functions
+
+# Returns new user ID
+add_user(username) -> int
+# Returns the new sequence ID
+add_sequence(sequence_name, user_id) -> int
+# Modifies key if already exists
+# Returns true if successful
+add_api_key(user_id: int, type: string, key: string) -> bool
+
+# If index < 0, segment is added to the beginning of the sequence
+# If index > max, segment is added to the end of the sequence
+# All versions start at 0, meaning NO version.
+# Function will ++ all >= indices to insert.
+# sequence_index unspecified will add to the end of the sequence.
+# Returns new segment ID
+add_segment(sequence_id, sequence_index = last) -> int
+
+# Setting "switch" to true will automatically select this new version in the segment.
+# Returns the version assigned.
+add_segment_element(segment_id: int, element: Element, content: str, switch: bool = false) -> int
+
+
+### Modify functions
+
+# Returns true if successful
+change_username(user_id) -> bool
+# Returns true if successful
+change_sequence_name(sequence_id) -> bool
+
+# Returns the new index, or false if unsucessful
+change_segment_index(segment_id, new_index) -> int | bool
+
+# No modifying segment elements, all "changes" are new versions.
+# For text, GUI will have a "lock in new version" button after modification
+
+
+### Get functions
+# All get functions will return false if the entry doesn't exist.
+
+get_id_from_username(username: str) -> int | False
+get_username_from_id(user_id: int) -> str | False
+
+get_api_key(user_id: int, type: str) -> str | False
+
+get_sequences(user_id: int) -> Sequence[]
+get_sequence(sequence_id: int) -> Sequence
+
+get_segment(segment_id: int) -> Segment
+get_segment_element(segment_id: int, element: Element, version: int = -1) -> str
+
+get_segment_count(sequence_id: int) -> int
+get_segment_element_version_count(segment_id: int, element: Element) -> int
+
+
+
+# Existential functions
+
+does_username_exist(username: str) -> bool
+does_user_id_exist(user_id: int) -> bool
+
+does_sequence_name_exist(user_id, sequence_name: str) -> bool
+does_sequence_id_exist(sequence_id: int) -> bool
+
+does_segment_id_exist(segment_id: int) -> bool
+does_sequence_index_exist(sequence_id: int, sequence_index: int) -> bool
+
+does_segment_element_version_exist(sequence_id: int, element: Element, version: int) -> bool
+
+
+-- Generate the script for a sequence automatically:
+-- Boy I hope this would work!
+SELECT GROUP_CONCAT(content, ' ')
+FROM segment_text
+INNER JOIN segments
+    ON segment_id = segments.id
+WHERE sequence_id = <?>
+    AND version = text_version
+ORDER BY sequence_index;
+
+
+'''
+
+
 import sqlite3
+from dataclasses import dataclass
+
+@dataclass
+class Sequence:
+    id: int
+    user_id: int
+    name: str
+    script: str | None = None
+
+@dataclass
+class Segment:
+    id: int
+    sequence_id: int
+    sequence_index: int
+    text_version: int
+    image_version: int
+    audio_version: int
+    
+
 
 # Initialize the database file
 con = sqlite3.connect('database.db')
@@ -67,21 +173,40 @@ def get_user_id_from_username(username):
                        ''', (username.lower(),))
     return ret.fetchall()[0][0]
 
-def get_sequence_names_by_user_id(user_id):
+def get_sequences_by_user_id(user_id):
     ret = cur.execute('''
-                       SELECT name FROM sequences
+                       SELECT id, name FROM sequences
                        WHERE user_id = ?
                        ''', (user_id,))
-    return [n[0] for n in ret.fetchall()]
+    return ret.fetchall()
 
+from enum import Enum
+class Element(Enum):
+    TEXT = "text"
+    IMAGE = "image"
+    AUDIO = "audio"
+
+def get_sequence_element(element: Element, sequence_id: int, sequence_index: int, version: int = -1):
+    if version == -1:
+        # If version is unspecified, get the version specified in segment.{element}_version
+        ret = cur.execute(f'''
+                        SELECT content FROM sequence_{element}
+                        INNER JOIN segments
+                            ON segment_id = segments.id
+                        WHERE sequence_id = ? 
+                            AND sequence_index = ?
+                            AND version = {element}_version;
+                        ''', (sequence_id, sequence_index))
+    else:
+        ret = cur.execute(f'''
+                        SELECT content FROM sequence_{element}
+                        INNER JOIN segments
+                            ON segment_id = segments.id
+                        WHERE sequence_id = ? 
+                            AND sequence_index = ?
+                            AND version = ?;
+                        ''', (sequence_id, sequence_index, version))
+    return ret.fetchall()[0][0]
 
 
 init_database()
-
-print(add_user("Caiden2000", "password123"))
-uid = get_user_id_from_username("Caiden2000")
-add_sequence(uid, "project1")
-add_sequence(uid, "project2")
-add_sequence(uid, "da_prOjeckt thr33")
-
-print(get_sequence_names_by_user_id(uid))
